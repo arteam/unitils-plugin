724b395adbe2da85dad15af4f654ae5c5562b089
arteam <arteamon@gmail.com>
2012-09-09 00:26:07 +0400

    Some description

--- src/com/intellij/unitils/annotators/SpringBeanReferenceAnnotator.java
+++ src/com/intellij/unitils/annotators/SpringBeanReferenceAnnotator.java
@@ -13,18 +13,16 @@ import com.intellij.spring.model.xml.beans.SpringBaseBeanPointer;
 import com.intellij.spring.model.xml.beans.SpringBeanPointer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import com.intellij.openapi.diagnostic.Logger;
 
 /**
  * spring bean reference annotator
  *
  * @author linux_china@hotmail.com
+ * @author ArTeam
  */
 public class SpringBeanReferenceAnnotator implements Annotator {
 
-    private static final Logger log = Logger.getInstance(SpringBeanReferenceAnnotator.class);
 
     /**
      * spring bean reference annotator
@@ -41,15 +39,10 @@ public class SpringBeanReferenceAnnotator implements Annotator {
         for (PsiClass javaClass : ((PsiJavaFile) psiElement).getClasses()) {
 
             PsiClass superClass = javaClass.getSuperClass();
-            log("Java class: " + javaClass.getText());
-            log("Super class: " + (superClass != null ? superClass.getText() : ""));
             PsiField[] superClassFields = superClass != null ? superClass.getAllFields() : new PsiField[]{};
-            log("Super class fields: " + Arrays.asList(superClassFields));
             for (PsiField field : javaClass.getAllFields()) {
                 // We don't need scan super class fields
-                log("Field: " + field.getNameIdentifier().getText());
                 if (isContain(superClassFields, field)) {
-                    log("Field: " + field.getNameIdentifier().getText() + " isn't needed");
                     continue;
                 }
 
@@ -68,6 +61,7 @@ public class SpringBeanReferenceAnnotator implements Annotator {
                     if (springModel == null) {
                         continue;
                     }
+
                     //by type
                     if (qcName.endsWith("SpringBeanByType")) {
                         final PsiType type = field.getType();
@@ -84,7 +78,6 @@ public class SpringBeanReferenceAnnotator implements Annotator {
                             for (SpringBaseBeanPointer springBaseBeanPointer : springBaseBeanPointerList) {
                                 elements.add(springBaseBeanPointer.getPsiElement());
                             }
-                            log("Target elements: " + elements);
                             NavigationGutterIconBuilder.create(SpringIcons.SPRING_BEAN_ICON)
                                     .setTooltipText("Navigate to the spring bean declaration(s)")
                                     .setTargets(elements)
@@ -126,13 +119,6 @@ public class SpringBeanReferenceAnnotator implements Annotator {
             }
         }
         return false;
-    }
-
-    /**
-     * Write fixed content to the given file.
-     */
-    void log(String text) {
-        //log.info(text);
     }
 
 }
